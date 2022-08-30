1294ce1e353483d6d6bc5eda23476d00f08240e4
liudong <liudong@zsoft.com.cn>
2022-08-30 16:04:19 +0800

    学习

--- src/main/java/com/dong/javaThink/tenth/Test.java
+++ src/main/java/com/dong/javaThink/tenth/Test.java
@@ -1,5 +1,11 @@
 package com.dong.javaThink.tenth;
 
+
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+
+import java.util.Date;
+
 /**
  * @author liudong 2022/8/25
  */
@@ -7,5 +13,10 @@ public class Test {
     public static void main(String[] args) {
         Parcel1 p = new Parcel1();
         Parcel1.Contents contents = p.new Contents();
+
+        Date date1 = DateUtil.parseDate("2022-08-10");
+        Date date2 = DateUtil.parseDate("2022-08-11");
+        boolean before = date2.before(date1);
+        System.out.println(before);
     }
 }

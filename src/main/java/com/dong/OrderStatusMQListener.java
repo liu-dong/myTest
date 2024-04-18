4e692533ab09ae7a6219857031c4fb508c8a7b03
liudong <964708273@qq.com>
2024-04-18 20:41:55 +0800

    学习枚举

--- src/main/java/com/dong/OrderStatusMQListener.java
+++ src/main/java/com/dong/OrderStatusMQListener.java
@@ -2,122 +2,222 @@ package com.dong;
 
 /**
  * 【建议拷贝到本地环境编写后贴回考试系统，考试结束会自动收卷，请自行定时将代码拷回到系统中】
- *
+ * <p>
  * 业务背景：这是一个可以售卖多种类目商品（酒店、门票、签证）的旅游类电商系统。 这个类中主要是接收到订单状态变更消息的逻辑处理。
  * 目前有订单支付（这里的支付是直接扣款，有免密协议）和订单完成两部分逻辑。
- *
+ * <p>
  * 题目要求：希望你能够将这段代码进行重构，目标：拥有好的可读性（清晰、直观、简洁）、
  * 可扩展性（面向未来商品类的扩展、订单的状态扩展、公共逻辑扩展（比如增加入参、出参日志打印和异常控制逻辑加入））
  * 题目判分的优先级： 设计思路 > 代码结构 > 规范性 > 可读性 > 完整度
  * 参考建议：
- *  1. 可读性：
- *     1）代码的注释、命名
- *     2）枚举类
- *     3）重复代码归纳合并
- *  2. 扩展性考虑：
- *     1）设计模式 工厂、模板方法
- *     2）事件驱动方式 比如 SpringEvent 或者 EventBus。没有用过可以上网查哦。（代码中体现出结构和大概逻辑即可）
+ * 1. 可读性：
+ * 1）代码的注释、命名
+ * 2）枚举类
+ * 3）重复代码归纳合并
+ * 2. 扩展性考虑：
+ * 1）设计模式 工厂、模板方法
+ * 2）事件驱动方式 比如 SpringEvent 或者 EventBus。没有用过可以上网查哦。（代码中体现出结构和大概逻辑即可）
  */
 public class OrderStatusMQListener {
     private OrderService orderService = new OrderService();
-    private PayService payService = new PayService();
     private NotifyService notifyService = new NotifyService();
+    private OrderStatusStrategyFactory strategyFactory = new OrderStatusStrategyFactory();
 
+    /**
+     * 消费接口
+     *
+     * @param orderNo 订单号
+     */
     public void consume(String orderNo) {
         final OrderInfo orderInfo = orderService.getOrderInfo(orderNo);
-        // 订单业务类型
-        final String orderBizType = orderInfo.getOrderBizType();
-        // 订单状态
-        final int status = orderInfo.getStatus();
-
-        if (1 == status) { // 创建，待支付执行支付
-            // 酒店订单
-            if ("hotel".equals(orderBizType)) {
-                // 酒店订单支付前的检查 ......
-                payPrdOfHotelVerify(orderInfo);
-                // 信用支付
-                long payId = payService.payOfCredit(orderInfo);
-                // 如果支付失败关闭订单
-                if(payId < 0) {
-                    payService.notPayedToClose(orderInfo);
-                }
-            }
-            // 门票订单
-            else if ("tickets".equals(orderBizType)) {
-                long payId = -1;
-                for (int i = 0; i < 3; i++) { // 如果失败有3次重试
-                    // 信用支付
-                    payId = payService.payOfCredit(orderInfo);
-                    // payId >0 代表支付成功
-                    if (payId > 0) {
-                        break;
-                    }
-                }
-                // 如果支付失败关闭订单
-                if(payId < 0) {
-                    payService.notPayedToClose(orderInfo);
-                }
-            }
-            // 签证订单
-            else if ("visa".equals(orderBizType)) {
-                // 三方支付
-                long payId = payService.payOfAliPay(orderInfo);
-                // 如果支付失败关闭订单
-                if(payId < 0) {
-                    payService.notPayedToClose(orderInfo);
-                }
-                // 如果支付成功
-                else {
-                    // 签证订单支付后业务上要处理的一些逻辑
-                    payAfterOfVisaBusiness(payId, orderInfo);
-                }
-            }
-        } else if (4 == status) { // 订单完成
-            // 酒店订单
-            if ("hotel".equals(orderBizType)) {
-                // 更新订单状态
-                orderService.updateOrderStatus(orderNo, 4);
-                // 给运营发钉钉通知
-                notifyService.dingding("发送到酒店运营群里的内容");
-                // 给用户发短信
-                notifyService.sms("发送到给酒店用户的内容");
-            }
-            // 门票订单
-            else if ("tickets".equals(orderBizType)) {
-                // 更新订单状态
-                orderService.updateOrderStatus(orderNo, 4);
-                // 给运营发钉钉通知
-                notifyService.dingding("发送到门票运营群里的内容");
-                // 给用户发短信
-                notifyService.sms("发送到给门票用户的内容");
-                // 给用户发微信通知
-                notifyService.wechat("发送到给门票用户的内容");
-            }
-            // 签证订单
-            else if ("visa".equals(orderBizType)) {
-                // 更新订单状态
-                orderService.updateOrderStatus(orderNo, 4);
-                // 给运营发钉钉通知
-                notifyService.dingding("发送到签证运营群里的内容");
-                // 给用户发微信通知
-                notifyService.wechat("发送到给签证用户的内容");
-            }
+        // 使用工厂类获取对应的处理策略
+        OrderStatusStrategy strategy = strategyFactory.getStrategy(orderInfo);
+        if (strategy != null) {
+            strategy.processOrder(orderInfo);
         }
-        // 同意退款 待未来业务扩展......
-        else if (9 == status) {
+    }
 
+}
+
+/**
+ * 不同订单业务类型工厂类
+ */
+class OrderBizTypeStrategyFactory {
+    public OrderBizTypeStrategy getPayStrategy(String orderBizType) {
+        switch (orderBizType) {
+            case "hotel":
+                return new HotelOrderBizTypeStrategy();
+            case "tickets":
+                return new TicketOrderBizTypeStrategy();
+            case "visa":
+                return new VisaOrderBizTypeStrategy();
+            default:
+                throw new IllegalArgumentException("Unknown order type: " + orderBizType);
         }
+    }
+}
 
+/**
+ * 不同订单业务类型策略接口
+ */
+interface OrderBizTypeStrategy {
+    /**
+     * 创建支付
+     *
+     * @param orderInfo
+     */
+    void processPayment(OrderInfo orderInfo);
+
+    /**
+     * 订单完成
+     *
+     * @param orderInfo
+     */
+    void processComplete(OrderInfo orderInfo);
+
+    // 退单。。。
+
+}
+
+/**
+ * 酒店订单业务类型
+ */
+class HotelOrderBizTypeStrategy implements OrderBizTypeStrategy {
+    private OrderService orderService = new OrderService();
+    private NotifyService notifyService = new NotifyService();
+    private PayService payService = new PayService();
+
+    /**
+     * 支付
+     *
+     * @param orderInfo
+     */
+    @Override
+    public void processPayment(OrderInfo orderInfo) {
+        // 酒店订单支付前的检查 ......
+        payPrdOfHotelVerify(orderInfo);
+        // 信用支付
+        long payId = payService.payOfCredit(orderInfo);
+        // 如果支付失败关闭订单
+        if (payId < 0) {
+            payService.notPayedToClose(orderInfo);
+        }
+    }
+
+    /**
+     * 订单完成
+     *
+     * @param orderInfo
+     */
+    @Override
+    public void processComplete(OrderInfo orderInfo) {
+        // 更新订单状态
+        orderService.updateOrderStatus(orderInfo.getOrderNo(), 4);
+        // 给运营发钉钉通知
+        notifyService.dingding("发送到酒店运营群里的内容");
+        // 给用户发短信
+        notifyService.sms("发送到给酒店用户的内容");
     }
 
     private void payPrdOfHotelVerify(OrderInfo orderInfo) {
         // 各种酒店订单支付前的检查 ...... 候选人不用关系内部逻辑
     }
+}
+
+/**
+ * 门票订单业务类型
+ */
+class TicketOrderBizTypeStrategy implements OrderBizTypeStrategy {
+    private OrderService orderService = new OrderService();
+    private NotifyService notifyService = new NotifyService();
+    private PayService payService = new PayService();
+
+    /**
+     * 支付
+     *
+     * @param orderInfo
+     */
+    @Override
+    public void processPayment(OrderInfo orderInfo) {
+        long payId = -1;
+        for (int i = 0; i < 3; i++) { // 如果失败有3次重试
+            // 信用支付
+            payId = payService.payOfCredit(orderInfo);
+            // payId >0 代表支付成功
+            if (payId > 0) {
+                break;
+            }
+        }
+        // 如果支付失败关闭订单
+        if (payId < 0) {
+            payService.notPayedToClose(orderInfo);
+        }
+    }
+
+    /**
+     * 订单完成
+     *
+     * @param orderInfo
+     */
+    @Override
+    public void processComplete(OrderInfo orderInfo) {
+        // 更新订单状态
+        orderService.updateOrderStatus(orderInfo.getOrderNo(), 4);
+        // 给运营发钉钉通知
+        notifyService.dingding("发送到门票运营群里的内容");
+        // 给用户发短信
+        notifyService.sms("发送到给门票用户的内容");
+        // 给用户发微信通知
+        notifyService.wechat("发送到给门票用户的内容");
+    }
+}
+
+/**
+ * 签证订单业务类型
+ */
+class VisaOrderBizTypeStrategy implements OrderBizTypeStrategy {
+    private OrderService orderService = new OrderService();
+    private NotifyService notifyService = new NotifyService();
+    private PayService payService = new PayService();
+
+    /**
+     * 支付
+     *
+     * @param orderInfo
+     */
+    @Override
+    public void processPayment(OrderInfo orderInfo) {
+        // 三方支付
+        long payId = payService.payOfAliPay(orderInfo);
+        // 如果支付失败关闭订单
+        if (payId < 0) {
+            payService.notPayedToClose(orderInfo);
+        } else {// 如果支付成功
+            // 签证订单支付后业务上要处理的一些逻辑
+            payAfterOfVisaBusiness(payId, orderInfo);
+        }
+    }
+
+    /**
+     * 订单完成
+     *
+     * @param orderInfo
+     */
+    @Override
+    public void processComplete(OrderInfo orderInfo) {
+        // 更新订单状态
+        orderService.updateOrderStatus(orderInfo.getOrderNo(), 4);
+        // 给运营发钉钉通知
+        notifyService.dingding("发送到签证运营群里的内容");
+        // 给用户发微信通知
+        notifyService.wechat("发送到给签证用户的内容");
+    }
+
     private void payAfterOfVisaBusiness(long payId, OrderInfo orderInfo) {
         // 各种签证订单支付后的逻辑 ...... 候选人不用关系内部逻辑
     }
-
 }
+
 
 /**
  * 订单状态策略工厂类
@@ -147,10 +247,14 @@ interface OrderStatusStrategy {
  */
 class PayStrategy implements OrderStatusStrategy {
     private PayService payService = new PayService();
+    private OrderBizTypeStrategyFactory strategyFactory = new OrderBizTypeStrategyFactory();
 
     @Override
     public void processOrder(OrderInfo orderInfo) {
-        // 支付逻辑，可以进一步细分为不同的支付方式策略
+        // 获取对应的订单业务类型策略
+        OrderBizTypeStrategy strategy = strategyFactory.getPayStrategy(orderInfo.getOrderBizType());
+        // 执行支付逻辑
+        strategy.processPayment(orderInfo);
     }
 }
 
@@ -158,12 +262,30 @@ class PayStrategy implements OrderStatusStrategy {
  * 订单完成状态业务
  */
 class CompleteStrategy implements OrderStatusStrategy {
-    private OrderService orderService = new OrderService();
+    private OrderBizTypeStrategyFactory strategyFactory = new OrderBizTypeStrategyFactory();
     private NotifyService notifyService = new NotifyService();
 
     @Override
     public void processOrder(OrderInfo orderInfo) {
-        // 订单完成的后续处理逻辑
+        // 获取对应的订单业务类型策略
+        OrderBizTypeStrategy strategy = strategyFactory.getPayStrategy(orderInfo.getOrderBizType());
+        // 执行订单完成逻辑
+        strategy.processComplete(orderInfo);
+        // 发送通知
+        notifyService.dingding("给运营发钉钉通知");
+        notifyService.sms("给用户发短信");
+        notifyService.wechat("给用户发微信通知");
+    }
+}
+
+/**
+ * 同意退款状态业务
+ */
+class RefundStrategy implements OrderStatusStrategy {
+
+    @Override
+    public void processOrder(OrderInfo orderInfo) {
+        // 退款业务逻辑
     }
 }
 
@@ -192,6 +314,7 @@ class PayService {
 
     /**
      * 信用支付
+     *
      * @param orderInfo
      */
     long payOfCredit(OrderInfo orderInfo) {
@@ -201,6 +324,7 @@ class PayService {
 
     /**
      * 支付宝支付
+     *
      * @param orderInfo
      */
     long payOfAliPay(OrderInfo orderInfo) {
@@ -210,6 +334,7 @@ class PayService {
 
     /**
      * 未支付成功后关闭
+     *
      * @param orderInfo
      */
     public void notPayedToClose(OrderInfo orderInfo) {
@@ -221,19 +346,26 @@ class NotifyService {
     /**
      * 短信通知
      */
-    void sms(String msg) {}
+    void sms(String msg) {
+    }
+
     /**
      * 钉钉通知
      */
-    void dingding(String msg) {}
+    void dingding(String msg) {
+    }
+
     /**
      * 微信通知
      */
-    void wechat(String msg) {}
+    void wechat(String msg) {
+    }
+
     /**
      * 邮件通知
      */
-    void email(String msg) {}
+    void email(String msg) {
+    }
 }
 
 
